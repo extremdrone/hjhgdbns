9d71e29df56a6005b21c3f6bce925d4b9763d225
liman <man.li@phicomm.com>
2018-02-05 20:36:14 +0800

    no message

--- Sources/Dotzu/Logger.swift
+++ Sources/Dotzu/Logger.swift
@@ -21,41 +21,48 @@ public func DebugManLog<T>(_ file: String = #file,
                            _ message: T,
                            _ color: UIColor? = nil)
 {
+    Swift.print(message)
     Logger.shared.handleLog(file: file, function: function, line: line, message: message, color: color)
-//    Swift.print(message)
 }
 
 //MARK: -
-public class Logger {
+public class Logger: NSObject {
     
     static let shared = Logger()
     
     fileprivate func parseFileInfo(file: String?, function: String?, line: Int?) -> String? {
         guard let file = file, let function = function, let line = line, let fileName = file.components(separatedBy: "/").last else {return nil}
-        
         return "\(fileName)[\(line)]\(function):\n"
     }
 
-    
     func handleLog(file: String?, function: String?, line: Int?, message: Any..., color: UIColor?) {
+        //1.
         let fileInfo = parseFileInfo(file: file, function: function, line: line)
         let stringContent = message.reduce("") { result, next -> String in
             return "\(result)\(result.count > 0 ? " " : "")\(next)"
         }
         
+        //2.
+        let newLog = Log(content: stringContent, color: color, fileInfo: fileInfo)
+        StoreManager.shared.addLog(newLog)
         
-        //liman
-        DispatchQueue.global().async {
-            //子线程
-            let newLog = Log(content: stringContent, color: color, fileInfo: fileInfo)
-            let format = LoggerFormat.format(newLog)
-            Swift.print(format.str)
-            StoreManager.shared.addLog(newLog)
-            
-            DispatchQueue.main.async {
-                //主线程
-                NotificationCenter.default.post(name: NSNotification.Name("refreshLogs_DebugMan"), object: nil, userInfo: nil)
-            }
+        dispatch_main_async_safe {
+            NotificationCenter.default.post(name: NSNotification.Name("refreshLogs_DebugMan"), object: nil, userInfo: nil)
         }
+        
+        
+        
+//        DispatchQueue.global().async {
+//            //子线程
+//            let newLog = Log(content: stringContent, color: color, fileInfo: fileInfo)
+//            let format = LoggerFormat.format(newLog)
+//            Swift.print(format.str)
+//            StoreManager.shared.addLog(newLog)
+//
+//            DispatchQueue.main.async {
+//                //主线程
+//                NotificationCenter.default.post(name: NSNotification.Name("refreshLogs_DebugMan"), object: nil, userInfo: nil)
+//            }
+//        }
     }
 }
